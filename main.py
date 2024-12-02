9654d71b4a3e4996d16033853f607f8a336604af
saimanoj <saimanoj@vt.edu>
2024-12-02 15:11:16 -0500

    year updates

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def valid_values():
         'address': ['123 Main St'],
         'phone': ['408-555-1212', '408 555 1212', '(408) 555 1212'],
         'ccNumber': ['4444333322221111'],
-        'ccExpiryMonth': [7],
+        'ccExpiryMonth': [12],
         'ccExpiryYear': [2026]
     }
 
@@ -89,8 +89,8 @@ def run_tests(host_url, student_name):
     points = 20  # Reset points for each student
     results = []
     today = datetime.date.today()
-    current_month = 12
-    current_year = 2024
+    current_month = today.month
+    current_year = today.year
 
     try:
         results.append(f"TESTING {student_name} - {host_url}: out of {total_points}\n")
